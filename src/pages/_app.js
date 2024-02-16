024da43cf06419c0e35c074253b07db048b90945
luangelsdorf <luanferreira2136@gmail.com>
2024-02-16 15:46:53 -0300

    Initial setup

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,6 @@
-import "@/styles/globals.css";
+import "@/styles/globals.scss";
+import 'bootstrap/dist/css/bootstrap-grid.min.css';
+
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
